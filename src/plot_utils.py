30a2572d53cbf5ec259eaa47209d64474deff8da
Sean Kao <seankao31@gmail.com>
2019-02-09 20:32:23 +0800

    plot_utils, task_view: displayVoronoi (edge only)
    voronoi_segments:
    return finite/infinite segments
    
    displayVoronoi:
    blend back_image with draw_voronoi

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,53 +1,9 @@
 import numpy as np
-from scipy.spatial._plotutils import _held_figure, _adjust_bounds
 
 
-@_held_figure
-def voronoi_plot_2d(vor, ax=None, **kw):
-    """
-    Plot the given Voronoi diagram in 2-D
-    Parameters
-    ----------
-    vor : scipy.spatial.Voronoi instance
-        Diagram to plot
-    ax : matplotlib.axes.Axes instance, optional
-        Axes to plot on
-    show_points: bool, optional
-        Add the Voronoi points to the plot.
-    show_vertices : bool, optional
-        Add the Voronoi vertices to the plot.
-    line_colors : string, optional
-        Specifies the line color for polygon boundaries
-    line_width : float, optional
-        Specifies the line width for polygon boundaries
-    line_alpha: float, optional
-        Specifies the line alpha for polygon boundaries
-    line_dashed: bool, optional
-        Display line to infinity as dashed line
-    Returns
-    -------
-    fig : matplotlib.figure.Figure instance
-        Figure for the plot
-    See Also
-    --------
-    Voronoi
-    Notes
-    -----
-    Requires Matplotlib.
-    """
-    from matplotlib.collections import LineCollection
-
+def voronoiSegments(vor, **kw):
     if vor.points.shape[1] != 2:
         raise ValueError("Voronoi diagram is not 2-D")
-
-    if kw.get('show_points', True):
-        ax.plot(vor.points[:, 0], vor.points[:, 1], '.')
-    if kw.get('show_vertices', True):
-        ax.plot(vor.vertices[:, 0], vor.vertices[:, 1], 'o')
-
-    line_colors = kw.get('line_colors', 'k')
-    line_width = kw.get('line_width', 1.0)
-    line_alpha = kw.get('line_alpha', 1.0)
 
     center = vor.points.mean(axis=0)
     ptp_bound = vor.points.ptp(axis=0)
@@ -71,20 +27,4 @@ def voronoi_plot_2d(vor, ax=None, **kw):
 
             infinite_segments.append([vor.vertices[i], far_point])
 
-    ax.add_collection(LineCollection(finite_segments,
-                                     colors=line_colors,
-                                     lw=line_width,
-                                     alpha=line_alpha,
-                                     linestyle='solid'))
-    infinite_linestyle = 'solid'
-    if kw.get('line_dashed', False):
-        infinite_linestyle = 'dashed'
-    ax.add_collection(LineCollection(infinite_segments,
-                                     colors=line_colors,
-                                     lw=line_width,
-                                     alpha=line_alpha,
-                                     linestyle=infinite_linestyle))
-
-    _adjust_bounds(ax, vor.points)
-
-    return ax.figure
+    return finite_segments, infinite_segments
--- src/task_view.py
+++ src/task_view.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from PIL import ImageTk
+from PIL import Image, ImageDraw, ImageTk
+
+from src.plot_utils import voronoiSegments
 
 
 class TaskView(tk.Toplevel):
@@ -10,11 +12,24 @@ class TaskView(tk.Toplevel):
         self.canvas.pack(side='left')
 
     def displayImage(self, image):
-        self.image = image
+        self.back_image = image
         self.photo_image = photo_image = ImageTk.PhotoImage(image)
-        width, height = image.size
+        self.width, self.height = width, height = image.size
         self.canvas.config(width=width, height=height)
         self.canvas.create_image((0, 0), image=photo_image, anchor="nw")
 
     def displayVoronoi(self, voronoi):
-        pass
+        self.draw_voronoi = Image.new("RGB", (self.width, self.height), 'white')
+        self.draw = ImageDraw.Draw(self.draw_voronoi)
+
+        finite_segments, infinite_segments = voronoiSegments(voronoi)
+        for s in finite_segments:
+            self.draw.line([s[0][0], s[0][1], s[1][0], s[1][1]], 'red')
+        for s in infinite_segments:
+            self.draw.line([s[0][0], s[0][1], s[1][0], s[1][1]], 'red')
+
+        blend = Image.blend(
+            self.back_image.convert('RGB'), self.draw_voronoi, 0.5)
+        self.photo_blend = ImageTk.PhotoImage(blend)
+
+        self.canvas.create_image((0, 0), image=self.photo_blend, anchor="nw")
